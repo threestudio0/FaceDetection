6f06303c1c49dcaed5521b3d0e74d7691e886e26
liangfeng <liangfeng@hozonauto.com>
2018-09-05 03:05:07 -0700

    use lib modfiy

---
+++ UseFaceDetection/build/CMakeFiles/UseFaceDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UseFaceDetection.dir/facedetection_camera.cpp.o"
+  "UseFaceDetection.pdb"
+  "UseFaceDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UseFaceDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
